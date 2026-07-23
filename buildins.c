29ccae7b25049a323ee84f2db339affed73dd1ed
Animesh-DS <Animeshsingh519@gmail.com>
2026-07-23 19:56:17 +0000

    Clear command Implemented

--- buildins.c
+++ buildins.c
@@ -113,6 +113,10 @@ int command_which(char** args,char** env){
     }
 }
 
+int command_clear(){
+    system("clear");
+}
+
 int command_help(){
     printf("Avaliable Commands :\n");
     printf("%-25s - %s\n","      cd <path to directory>","Changes the current directory");
@@ -121,6 +125,7 @@ int command_help(){
     printf("%-25s - %s\n","      echo <text>","Prints the given text");
     printf("%-25s - %s\n","      which","Locates an executable in the system's path");
     printf("%-25s - %s\n","      help","Displays this help message");
+    printf("%-25s - %s\n","      clear","Clears the terminal");
     printf("%-25s - %s\n","      exit","Exit the shell");
 
     return 0;
