3414129ac18efcf0e3c31a6887b9483c2fb2f5b6
Animesh-DS <Animeshsingh519@gmail.com>
2026-07-23 18:48:23 +0000

    complete env and which base structure Implemented

--- buildins.c
+++ buildins.c
@@ -42,3 +42,75 @@ int command_env(char** env){
     printf("\n");
     return 0;
 }
+
+//echo HI, echo -n HI, echo $PATH
+int command_echo(char** args,char** env){
+    
+    int new_line = 1;
+    size_t i=1;    
+
+    if(args[1]!=NULL && strcmp(args[1],"-n")==0){
+        new_line=0;
+        i++;
+    }
+
+    //process remaining args using loop
+    for(;args[i];i++){
+        if(args[i][0] == '$'){
+            //handle env variables and then print path
+            char* path = getenv(args[i]+1);
+            if(path!=NULL){
+                printf("%s",path);
+            }
+        }
+        else{
+            printf("%s",args[i]);
+        }
+
+        if(args[i+1]!=NULL){ 
+            new_line=0;
+            printf(" ");
+        }
+
+        if(new_line) printf("\n");
+    }
+    return 0;
+}
+
+
+//external function path finder in PATH
+char* find_cmd_path(const char* command,char** env){
+    
+}
+
+int command_which(char** args,char** env){
+
+    if(args[1]==NULL){
+        printf("Expected arguments with \'which\'\n");
+        return 1;
+    }
+
+    //build in cmd
+    const char* build_in_cmds[] = {"cd","pwd","echo","env","setenv","unsetenv","which","exit",NULL};
+    for (size_t i = 0; build_in_cmds[i]; i++){
+        if(strcmp(args[1],build_in_cmds[i])==0){
+            printf("Shells Build in Command %s\n",args[1]);
+            return 0;
+        }
+    }
+    
+    //external cmd
+    char* path = find_cmd_path(args[1],env);
+    
+    if(path!=NULL){
+        printf("PATH : %s\n",path);
+        free(path);
+        return 0;
+    }
+    
+    if(path==NULL){
+        printf("Path Not Found\n");
+    }
+}
+
+
